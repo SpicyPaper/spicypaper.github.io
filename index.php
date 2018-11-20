6d65176b6bd038f1207b7d615cd02d28f5cdc1b3
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2018-11-20 17:07:05 +0100

    Add index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Spicy Paper</title>
+</head>
+<body>
+    <h1>Spicy Paper</h1>
+    <h2>Demo Projects</h2>
+    <a href="./fourmis">Fourmis project - Algo Num - HE-Arc - 2018</a>
+</body>
+</html>
